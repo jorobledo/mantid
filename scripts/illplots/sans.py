5f1104e69eb66546b1a08ead739e8aceae4d567b
Guillaume Communie <communieg@ill.eu>
2021-02-11 10:04:45 +0100

    [ILLPLOTS] sans submodule

---
+++ scripts/illplots/sans.py
@@ -0,0 +1,14 @@
+# Mantid Repository : https://github.com/mantidproject/mantid
+#
+# Copyright &copy; 2021 ISIS Rutherford Appleton Laboratory UKRI,
+#     NScD Oak Ridge National Laboratory, European Spallation Source
+#     & Institut Laue - Langevin
+# SPDX - License - Identifier: GPL - 3.0 +
+
+
+def plotIQ(wsName, filename=None):
+    pass
+
+
+def plotIQxQy(wsName, filename=None):
+    pass
